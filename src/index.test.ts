af5e7a472c10225bc38abebd7cb1111e91524acc
Olof Larsson <olof@folof.se>
2025-05-25 12:17:11 +0200

    fix: add missing tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,13 @@
 import { describe, expect, it } from 'vitest';
-import { deriveAM } from '.';
+import { deriveAMFromHex, padAM } from '.';
 
 describe('verify that the code is returning the correct result', () => {
   it('should return the correct result', () => {
     const uuid = 'ab:cd:ef:00';
-    const result = deriveAM(uuid);
+    const amResult = deriveAMFromHex(uuid);
+    const paddedResult = padAM(amResult);
 
-    expect(result).toBe('abcdef00');
+    expect(amResult).toBe('2882400000');
+    expect(paddedResult).toBe('00000002882400000');
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
-export function deriveAM(uidHex: string): string {
+export function deriveAMFromHex(uidHex: string): string {
   const uidString = `0x${uidHex.split(':').join('')}`;
   return Number(uidString).toString(10);
 }
+export function padAM(am: string, length = 17): string {
+  return am.padStart(length, '0');
+}
 
 const UUID = '00:00:00:00'; // Change this to the uuid to derive
-const am = deriveAM(UUID);
-const paddedAM = am.padStart(17, '0');
+const am = deriveAMFromHex(UUID);
+const paddedAM = padAM(am);
 
 console.log(`AM: ${am}`);
 console.log(`Padded AM: ${paddedAM}`);
